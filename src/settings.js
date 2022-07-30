410d44afdf3b3e82e200c4a361c9b4393d784fc2
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-07-30 20:31:09 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // 网站名字
+  title: 'hrsaas',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 控制左侧logo
+  sidebarLogo: true
+}
