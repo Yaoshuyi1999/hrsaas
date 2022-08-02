ec342588a9b5d422938ae15a3cc4d4b686887f37
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-08-02 10:50:34 +0800

    完成登录流程

--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,14 @@
 import request from '@/utils/request'
 
+/**
+ * 登录请求
+ * @param {Object} data password mobile 
+ * @return promise
+ */
 export function login(data) {
+    return request({
+        url: '/sys/login',
+        method: 'POST',
+        data,
+    })
 }
