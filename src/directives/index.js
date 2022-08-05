a877797ff133f9f59ea21b687eb8c73a035301a8
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-08-05 11:32:21 +0800

    结构完成

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,6 +1,7 @@
 export const imgError={
     // 当被绑定的元素插入到DOM 中时
     inserted:function(el,{value}){
+      console.log(el);
       el.onerror=function(){
         el.src=value
       }
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,21 +1,44 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <el-card class="box-card">
+        <!--头部-->
+        <tree-tools :treeNode="company" :isRoot="true"></tree-tools>
+        <!-- 树形结构 -->
+        <el-tree :data="treeData" :props="defaultProps" default-expand-all>
+          <template v-slot="{ data }">
+            <tree-tools :treeNode="data"></tree-tools>
+          </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from "./components/tree-tools.vue";
 export default {
+  components: {
+    TreeTools,
+  },
   data() {
-    return {}
+    return {
+      defaultProps: {
+        label: "name",
+      },
+      company: { name: "传智教育", manager: "负责人" },
+      treeData: [
+        { name: "总裁办", children: [{ name: "董事会" }] },
+        { name: "行政部" },
+        { name: "人事部" },
+      ],
+    };
   },
 
   created() {},
 
-  methods: {}
-}
+  methods: {},
+};
 </script>
 
 <style scoped lang="less"></style>
