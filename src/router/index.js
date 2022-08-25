cb752cd00cfdebc6bdb4ade4a95cac76ed4f1850
Yaoshuyi1999 <yaoshuyi19990118@163.com>
2022-08-25 10:19:47 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -59,7 +59,7 @@ export const asyncRoutes = [
 
 const createRouter = () =>
   new Router({
-    // mode: 'history', // require service support
+    mode: 'hash', // require service support
     scrollBehavior: () => ({ y: 0 }),
     routes: [...constantRoutes], // 路由规则
   })
